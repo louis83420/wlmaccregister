70fcb1f78c175412c2b4fa5dd5c203e8305a0065
Joy <louis83420@gmail.com>
2024-06-30 12:58:47 +0800

    帳號密碼按鈕位置額外放

--- ipyautogui2.py
+++ ipyautogui2.py
@@ -1,5 +1,14 @@
 import pyautogui
 import time
+import json
+
+# 讀取配置文件
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+account = config['account']
+password = config['password']
+coords = config['coordinates']
 
 def switch_to_english_input():
     # 切換輸入法到英語（模擬按下 Alt + Shift）
@@ -11,20 +20,20 @@ def create_account():
     switch_to_english_input()
     
     # 點擊登入按鈕
-    pyautogui.click(login_x, login_y)
+    pyautogui.click(*coords['login'])
     time.sleep(1)  # 根據需要調整等待時間
 
     # 點擊創帳號按鈕
-    pyautogui.click(register_x, register_y)
+    pyautogui.click(*coords['register'])
     time.sleep(1)
 
     # 在acc創建帳號欄位輸入account的內容
-    pyautogui.click(acc_x, acc_y)
+    pyautogui.click(*coords['acc_create'])
     pyautogui.typewrite(account[0], interval=0.1)
     time.sleep(1)
 
     # 點擊acc創建確認按鈕
-    pyautogui.click(acclick_x, acclick_y)
+    pyautogui.click(*coords['acc_confirm'])
     time.sleep(1)
 
 def enter_promo_codes():
@@ -32,22 +41,17 @@ def enter_promo_codes():
     promo_codes = ["KFGKUJC5R6", "477MQL98YA", "WP6YN23J96", "QGTBR4M4B3", "EZCHE6BAGS", "5VQVYE2NJY", "HTYXCTYY8B", "HN4PDR52VY", "NXAXZERBU5", "K4RQAK6AJ7", "HCNM9VHANB"]
 
     # 點擊系統按鈕
-    pyautogui.click(system_x, system_y)
+    pyautogui.click(*coords['system'])
     time.sleep(1)
     
     # 點擊序號兌換按鈕
-    pyautogui.click(serial_exchange_x, serial_exchange_y)
+    pyautogui.click(*coords['serial_exchange'])
     time.sleep(1)
 
-    # 輸入框位置
-    x, y = 1521, 1000  # 替換為你的輸入框的實際位置
-    # 確定按鈕位置
-    confirm_x, confirm_y = 1560, 1135  # 替換為你的“確定”按鈕的實際位置
-    
     # 迭代輸入每個優惠碼
     for promo_code in promo_codes:
         # 移動滑鼠到輸入框位置並點擊
-        pyautogui.click(x, y)
+        pyautogui.click(*coords['promo_input'])
         
         # 清空輸入框
         pyautogui.hotkey('ctrl', 'a')
@@ -57,7 +61,7 @@ def enter_promo_codes():
         pyautogui.typewrite(promo_code, interval=0.1)
 
         # 點擊確定按鈕
-        pyautogui.click(confirm_x, confirm_y)
+        pyautogui.click(*coords['promo_confirm'])
 
         # 等待一段時間以確保操作完成（根據需要調整時間）
         time.sleep(1)
@@ -67,41 +71,41 @@ def delete_account():
     switch_to_english_input()
     
     # 點擊系統按鈕
-    pyautogui.click(system_x, system_y)
+    pyautogui.click(*coords['system'])
     time.sleep(1)
 
     # 點擊刪除帳號按鈕
-    pyautogui.click(delete_account_x, delete_account_y)
+    pyautogui.click(*coords['delete_account'])
     time.sleep(1)
 
     # 點擊輸入密碼欄位並輸入密碼
-    pyautogui.click(password_input_x, password_input_y)
+    pyautogui.click(*coords['password_input'])
     pyautogui.typewrite(password[0], interval=0.1)
     time.sleep(1)
 
     # 點擊確認密碼欄位並輸入密碼
-    pyautogui.click(confirm_password_input_x, confirm_password_input_y)
+    pyautogui.click(*coords['confirm_password_input'])
     pyautogui.typewrite(password[0], interval=0.1)
     time.sleep(1)
 
     # 點擊刪除確認按鈕
-    pyautogui.click(confirm_deletion_x, confirm_deletion_y)
+    pyautogui.click(*coords['confirm_deletion'])
     time.sleep(1)
     
     # 點擊確認刪除
-    pyautogui.click(final_confirm_deletion_x, final_confirm_deletion_y)
+    pyautogui.click(*coords['final_confirm_deletion'])
     time.sleep(1)
     
     # 點擊刪除後確認返回登入頁
-    pyautogui.click(return_to_login_x, return_to_login_y)
+    pyautogui.click(*coords['return_to_login'])
     time.sleep(1)
     
     # 點擊清除快取
-    pyautogui.click(clear_cache_x, clear_cache_y)
+    pyautogui.click(*coords['clear_cache'])
     time.sleep(1)
     
     # 點擊確認清除快取
-    pyautogui.click(confirm_clear_cache_x, confirm_clear_cache_y)
+    pyautogui.click(*coords['confirm_clear_cache'])
     time.sleep(1)
 
 # 主程式
@@ -111,40 +115,6 @@ if __name__ == "__main__":
     print("2. 輸入優惠碼")
     print("3. 刪除帳號")
     choice = input("請輸入選擇的功能號碼：")
-
-    # 登入按鈕位置
-    login_x, login_y = 2286, 1404
-    # 創帳號按鈕位置
-    register_x, register_y = 1509, 1222
-    # 輸入帳號
-    account = ["joyjoyj"]
-    # acc創建欄位位置
-    acc_x, acc_y = 1640, 852
-    # acc創建確認按鈕位置
-    acclick_x, acclick_y = 1539, 1159
-
-    # 系統按鈕位置
-    system_x, system_y = 2397, 1414
-    # 序號兌換按鈕位置
-    serial_exchange_x, serial_exchange_y = 1050, 1343
-    # 刪除帳號按鈕位置
-    delete_account_x, delete_account_y = 1989, 799
-    # 輸入密碼欄位位置
-    password_input_x, password_input_y = 1629, 868
-    # 確認密碼欄位位置
-    confirm_password_input_x, confirm_password_input_y = 1627, 988
-    # 刪除確認按鈕位置
-    confirm_deletion_x, confirm_deletion_y = 1361, 1104
-    # 確認刪除
-    final_confirm_deletion_x, final_confirm_deletion_y = 1343, 1083
-    # 刪除後確認返回登入頁
-    return_to_login_x, return_to_login_y = 1512, 731
-    # 清除快取
-    clear_cache_x, clear_cache_y = 723, 1263
-    # 確認清除快取
-    confirm_clear_cache_x, confirm_clear_cache_y = 1412, 740
-    # 密碼
-    password = ["joyjoy"]
 
     if choice == "1":
         create_account()
